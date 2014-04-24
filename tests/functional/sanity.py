466dc8bca9530d58c3c3f07c3a6193d3824a03e7
Fabian Deutsch <fabiand@fedoraproject.org>
2014-04-24 15:22:19 +0200

    test: Improve functional testing
    Now a 9p fs is used to enable file exchanges between the host and guest.
    This allows to easily write tests on the host side and run them inside
    the guest.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/functional/sanity.py
+++ tests/functional/sanity.py
@@ -1,39 +1,81 @@
 
 import os
+import tempfile
 import unittest
 
 from virtexpect import NodeInstance
 
-IMAGE = os.environ["VIRTEXPECT_IMAGE"]
+IMAGE = os.environ.get("VIRTEXPECT_IMAGE", None)
 
 
-class TestSanity(unittest.TestCase):
+class NodeTestCase(unittest.TestCase):
+    node = None
+
+    def setUp(self):
+        shared_path = tempfile.mkdtemp(suffix="virtpexp")
+        self.node = NodeInstance(IMAGE, shared_path)
+        self.node.spawn()
+
+    def tearDown(self):
+        if self.node:
+            self.node.poweroff()
+
+    def log(self, msg):
+        return self.node.log.info(msg)
+
+    def sendline(self, txt):
+        return self.node.child.sendline(txt)
+
+    def expect(self, patterns, *args, **kwargs):
+        return self.node.child.expect(patterns, *args, **kwargs)
+
+
+class TestSanity(NodeTestCase):
     """Do some sanity testing on the image
     """
     def test_boot(self):
-        with NodeInstance(IMAGE) as node:
-            node.login()
+        self.log("Waiting for grub prompt")
+        self.expect("Fedora, with Linux")
+
+        self.log("Waiting for kernel output")
+        self.expect("Booting")
+
+        self.node.login()
+
+    def test_9p(self):
+        self.node.login()
+        self.node.mount_hostos()
+
+        self.log("Creating file on the host side")
+        hostfile = self.node.shared_path + "/bar"
+        os.system("echo echo Hi > %s" % hostfile)
+        os.system("chmod a+x %s" % hostfile)
+
+        self.log("Checking file on the guest side")
+        self.node.enter_shared_path()
+        self.sendline("./bar")
+        self.expect("Hi")
+
+        os.system("rm -f %s" % hostfile)
 
 
-class TestImgbase(unittest.TestCase):
+class TestImgbase(NodeTestCase):
     """Do sanity testing of the imgbase tool
     """
     def test_imgbase(self):
-        with NodeInstance(IMAGE) as node:
-            child = node.child
-            node.login()
+        self.node.login()
 
-            child.sendline('lvs')
-            child.expect('# ')
-            child.sendline('lvs HostVG/Layer-0.0')
-            child.expect('Layer-0.0', timeout=5)
-            child.expect('# ')
-            child.sendline('imgbase layer --add')
-            child.expect('Adding a new layer')
-            child.expect('Updating fstab')
-            child.expect('# ')
-            child.sendline('lvs HostVG/Layer-0.1')
-            child.expect('Layer-0.1', timeout=5)
+        self.sendline('lvs')
+        self.expect('# ')
+        self.sendline('lvs HostVG/Layer-0.0')
+        self.expect('Layer-0.0', timeout=5)
+        self.expect('# ')
+        self.sendline('imgbase layer --add')
+        self.expect('Adding a new layer')
+        self.expect('Updating fstab')
+        self.expect('# ')
+        self.sendline('lvs HostVG/Layer-0.1')
+        self.expect('Layer-0.1', timeout=5)
 
 
 if __name__ == '__main__':
--- tests/functional/virtexpect.py
+++ tests/functional/virtexpect.py
@@ -15,34 +15,64 @@ class Instance(object):
 
     child = None
 
-    def __init__(self, image):
+    mount_tag = "hostos"
+    shared_path = None
+    guestpath_to_host = "/tmp/%s" % mount_tag
+
+    def __init__(self, image, shared_path=None):
         assert image
         self.image = image
+        self.shared_path = shared_path
         self.log = logging.getLogger(__name__)
 
     def spawn(self):
-        child = pexpect.spawn(self.qemu_cmd(self.image))
+        cmd = self.qemu_cmd(self.image)
+        self.log.debug("Launching: %s" % cmd)
+        child = pexpect.spawn(cmd)
         child.logfile = sys.stdout
-        return child
+        child.setecho(False)
+        self.child = child
 
     def qemu_cmd(self, image):
         cmd = "qemu-kvm"
         cmd += " -m 1024 -serial stdio -net user -net nic"
         cmd += " -snapshot -hda %s" % image
         cmd += " -watchdog-action poweroff"
-        #cmd += " -virtfs fsdriver,id=bar01,path=%s,
-        #mount_tag=hostos,security_model=none" % shared_path
+
+        if self.shared_path:
+            self.log.debug("Using shared path: %s" % self.shared_path)
+            cmd += " -virtfs local,id=virtpexpect,"
+            cmd += "path=%s," % self.shared_path
+            cmd += "mount_tag=%s," % self.mount_tag
+            cmd += "security_model=none"
+
         return cmd
+
+    def _wait_command(self, cmd):
+        self.child.sendline(cmd)
+        self.child.expect("# ")  # ("^\[.*\]#")
+
+    def mount_hostos(self):
+        self._wait_command("mkdir %s" % self.guestpath_to_host)
+        self._wait_command("mount -t 9p -o trans=virtio %s %s" %
+                           (self.mount_tag, self.guestpath_to_host))
+        self._wait_command("findmnt %s" % self.guestpath_to_host)
+
+    def enter_shared_path(self):
+        self._wait_command("cd %s" % self.guestpath_to_host)
+
 
 class NodeInstance(Instance):
     """Makes more assumptions
     """
-
     def __enter__(self):
-        self.child = self.spawn()
+        self.spawn()
         return self
 
     def __exit__(self, *args, **kwargs):
+        self.poweroff()
+
+    def poweroff(self):
         if self.child.isalive():
             self.child.sendline('init 0')
             self.child.close()
@@ -58,3 +88,7 @@ class NodeInstance(Instance):
         self.child.expect('(?i)password:')
         self.child.sendline('r')
         self.child.expect('# ')
+
+    def logout(self):
+        self.child.sendline('exit')
+        self.child.expect('(?i)login:')
