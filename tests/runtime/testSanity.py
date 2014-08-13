8081220a84e6aebfb251b7b4249ddaba85ddfe99
Fabian Deutsch <fabiand@fedoraproject.org>
2014-08-13 09:07:26 +0200

    test: Log more stderr/stdout in nosetests
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/runtime/testSanity.py
+++ tests/runtime/testSanity.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 import unittest
+import sh
+
+
+# Increase the capture length of python-sh to show complete errors
+sh.ErrorReturnCode.truncate_cap = 999999
 
 
 class TestSanity(unittest.TestCase):
     def test_imgbase(self):
+
         from sh import imgbase
 
         assert "Image-0.0" in imgbase.layout()
