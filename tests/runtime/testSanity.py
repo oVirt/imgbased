509614b5d46a4cd948aa2a3373117c55f393516d
Fabian Deutsch <fabiand@fedoraproject.org>
2014-08-13 09:19:51 +0200

    test: Some more context for debugging
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/runtime/testSanity.py
+++ tests/runtime/testSanity.py
@@ -2,16 +2,23 @@
 
 import unittest
 import sh
+import logging
 
 
 # Increase the capture length of python-sh to show complete errors
 sh.ErrorReturnCode.truncate_cap = 999999
 
+log = logging.info
+
 
 class TestSanity(unittest.TestCase):
     def test_imgbase(self):
+        from sh import imgbase, lvm
 
-        from sh import imgbase
+        log("Existing LVM layout")
+        log(lvm.pvs())
+        log(lvm.vgs())
+        log(lvm.lvs())
 
         assert "Image-0.0" in imgbase.layout()
 
