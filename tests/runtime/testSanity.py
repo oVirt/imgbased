86369e12a910601e910bc3e753eadbe770eab7ff
Fabian Deutsch <fabiand@fedoraproject.org>
2014-08-15 18:33:29 +0200

    Move imgbased into place and fix testsing
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/runtime/testSanity.py
+++ tests/runtime/testSanity.py
@@ -15,12 +15,20 @@ class TestSanity(unittest.TestCase):
     def test_imgbase(self):
         from sh import imgbase, lvm
 
+        log("Using %s" % imgbase)
+        log(imgbase("--version"))
+
         log("Existing LVM layout")
         log(lvm.pvs())
         log(lvm.vgs())
         log(lvm.lvs())
 
+        assert "HostVG" in lvm.vgs()
+
+        # All subsequent imgbase calls include the debug arg
+        imgbase = imgbase.bake("--debug")
+
         assert "Image-0.0" in imgbase.layout()
 
-        imgbase.layer("--add")
+        log(imgbase.layer("--add"))
         assert "Image-0.1" in imgbase.layout()
