d2d02f5357603d5408e719953708deafc917a8b9
Fabian Deutsch <fabiand@fedoraproject.org>
2014-10-14 15:48:56 +0200

    plugins: Add kexec stub
    The idea of the kexec plugins is to be able to reboot into an image
    quickly.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ src/imgbased/plugins/kexec.py
@@ -0,0 +1,23 @@
+
+
+from ..utils import log
+
+
+def init(app):
+    app.hooks.connect("pre-arg-parse", add_argparse)
+    app.hooks.connect("post-arg-parse", check_argparse)
+
+
+def add_argparse(app, parser, subparsers):
+    s = subparsers.add_parser("kexec",
+                              help="Boot into an image")
+    s.add_argument("IMAGE", help="Image to use")
+
+
+def check_argparse(app, args):
+    log().debug("Operating on: %s" % app.imgbase)
+    if args.command == "kexec":
+        if args.image:
+            raise NotImplemented()
+
+# vim: sw=4 et sts=4
