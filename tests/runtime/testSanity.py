a10145401336e824f0c2514384fdc30f558762e1
Fabian Deutsch <fabiand@fedoraproject.org>
2014-10-14 10:32:01 +0200

    test: Move storage related test into module
    Storage related tests are now in a module, that way we can be more
    selective when running sanity checks.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/runtime/testSanity.py
+++ tests/runtime/testSanity.py
@@ -4,7 +4,6 @@
 import unittest
 import sh
 import logging
-import glob
 
 
 # Increase the capture length of python-sh to show complete errors
@@ -39,49 +38,6 @@ class TestImgbased(unittest.TestCase):
         diff = imgbase("--debug", "diff", "Image-0.0", "Image-0.1")
         print diff
         assert "/var/tmp/marker" in diff
-
-    def test_fstrim(self, use_autodiscard=False, num_trials=2, max_delta=0.05):
-        # FIXME improve by splitting into several test cases
-        from sh import imgbase, dd, sleep, rm, ls
-
-        imgbase = imgbase.bake("--debug")
-        img_free = lambda: float(imgbase("layout", "--free-space"))
-
-        def trials(num_bins=1, size_bin=500):
-            for N in xrange(0, num_trials):
-                a = img_free()
-
-                for B in xrange(0, num_bins):
-                    dd("if=/dev/zero", "of=/var/tmp/%s.bin" % N, "bs=1M",
-                       "count=%d" % size_bin)
-
-                print (ls("-shal", *glob.glob("/var/tmp/*.bin")))
-                b = img_free()
-
-                rm("-f", "/var/tmp/*.bin")
-
-                if use_autodiscard:
-                    print("We are testing the auto-discard " +
-                          "functionality of the fs")
-                    sleep("10")
-                else:
-                    from sh import fstrim
-                    fstrim("-v", "/")
-
-                c = img_free()
-
-                ratio = a / c
-
-                print(a, b, c, ratio)
-
-                assert ratio > (1.0 - max_delta), \
-                    "Delta is larger than %s" % max_delta
-
-        trials(1, 100)
-        trials(100, 1)
-
-    def test_discard(self):
-        return self.test_fstrim(use_autodiscard=True)
 
 
 class TestEnvironment(unittest.TestCase):
---
+++ tests/runtime/testStorage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# vim: et ts=4 sw=4 sts=4
+
+import unittest
+import sh
+import logging
+import glob
+
+
+# Increase the capture length of python-sh to show complete errors
+sh.ErrorReturnCode.truncate_cap = 999999
+
+log = logging.info
+
+
+class TestFS(unittest.TestCase):
+    def test_fstrim(self, use_autodiscard=False, num_trials=2, max_delta=0.05):
+        # FIXME improve by splitting into several test cases
+        from sh import imgbase, dd, sleep, rm, ls
+
+        imgbase = imgbase.bake("--debug")
+        img_free = lambda: float(imgbase("layout", "--free-space"))
+
+        def trials(num_bins=1, size_bin=500):
+            for N in xrange(0, num_trials):
+                a = img_free()
+
+                for B in xrange(0, num_bins):
+                    dd("if=/dev/zero", "of=/var/tmp/%s.bin" % N, "bs=1M",
+                       "count=%d" % size_bin)
+
+                print (ls("-shal", *glob.glob("/var/tmp/*.bin")))
+                b = img_free()
+
+                rm("-f", "/var/tmp/*.bin")
+
+                if use_autodiscard:
+                    print("We are testing the auto-discard " +
+                          "functionality of the fs")
+                    sleep("10")
+                else:
+                    from sh import fstrim
+                    fstrim("-v", "/")
+
+                c = img_free()
+
+                ratio = a / c
+
+                print(a, b, c, ratio)
+
+                assert ratio > (1.0 - max_delta), \
+                    "Delta is larger than %s" % max_delta
+
+        trials(1, 100)
+        trials(100, 1)
+
+    def test_discard(self):
+        return self.test_fstrim(use_autodiscard=True)
