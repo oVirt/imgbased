dc41c575ee2030bbca419721fc322c690b081d43
Fabian Deutsch <fabiand@fedoraproject.org>
2014-10-15 21:42:57 +0200

    test: Fix fstrim/discard tests
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/runtime/testStorage.py
+++ tests/runtime/testStorage.py
@@ -13,47 +13,56 @@ sh.ErrorReturnCode.truncate_cap = 999999
 log = logging.info
 
 
+def dd(N, count=100):
+    from sh import dd
+    dd("if=/dev/zero", "of=/var/tmp/%s.bin" % N, "bs=1M",
+       "count=%d" % count)
+
+
+def trial(num_bins=1, size_bin=500, after_rm=None, max_delta=0.05):
+    from sh import imgbase, rm, ls
+
+    imgbase = imgbase.bake("--debug")
+    img_free = lambda: float(imgbase("layout", "--free-space"))
+
+    a = img_free()
+
+    [dd(B, size_bin) for B in xrange(0, num_bins)]
+    print (ls("-shal", *glob.glob("/var/tmp/*.bin")))
+    b = img_free()
+
+    rm("-f", "/var/tmp/*.bin")
+    after_rm()
+    c = img_free()
+
+    ratio = a / c
+    print(a, b, c, ratio)
+    delta = 1 - ratio
+    assert delta < max_delta, \
+        "Delta %s is larger than %s" % (delta, max_delta)
+
+
 class TestFS(unittest.TestCase):
-    def test_fstrim(self, use_autodiscard=False, num_trials=2, max_delta=0.05):
+    def test_fstrim(self, count=1, size=100):
         # FIXME improve by splitting into several test cases
-        from sh import imgbase, dd, sleep, rm, ls
 
-        imgbase = imgbase.bake("--debug")
-        img_free = lambda: float(imgbase("layout", "--free-space"))
+        def after_rm():
+            from sh import fstrim
+            fstrim("-v", "/")
 
-        def trials(num_bins=1, size_bin=500):
-            for N in xrange(0, num_trials):
-                a = img_free()
+        trial(count, size, after_rm)
 
-                for B in xrange(0, num_bins):
-                    dd("if=/dev/zero", "of=/var/tmp/%s.bin" % N, "bs=1M",
-                       "count=%d" % size_bin)
+    def test_fstrim_many(self):
+        self.test_fstrim(100, 1)
 
-                print (ls("-shal", *glob.glob("/var/tmp/*.bin")))
-                b = img_free()
+    def test_discard(self, count=1, size=100):
+        def after_rm():
+            from sh import sleep
+            print("We are testing the auto-discard " +
+                  "functionality of the fs")
+            sleep("10")
 
-                rm("-f", "/var/tmp/*.bin")
+        trial(count, size, after_rm)
 
-                if use_autodiscard:
-                    print("We are testing the auto-discard " +
-                          "functionality of the fs")
-                    sleep("10")
-                else:
-                    from sh import fstrim
-                    fstrim("-v", "/")
-
-                c = img_free()
-
-                ratio = a / c
-
-                print(a, b, c, ratio)
-
-                delta = 1 - ratio
-                assert delta > max_delta, \
-                    "Delta %s is larger than %s" % (delta, max_delta)
-
-        trials(1, 100)
-        trials(100, 1)
-
-    def test_discard(self):
-        return self.test_fstrim(use_autodiscard=True)
+    def test_discard_many(self):
+        self.test_discard(100, 1)
