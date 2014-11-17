3f132ecbf6e291a3ce8ee3be3df8ccd22c49a133
Fabian Deutsch <fabiand@fedoraproject.org>
2014-11-17 21:05:18 +0100

    imgbase: Add layout plugin stub
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ src/imgbased/plugins/layout.py
@@ -0,0 +1,23 @@
+
+from ..utils import log
+
+
+def init(app):
+    app.hooks.connect("pre-arg-parse", add_argparse)
+    app.hooks.connect("post-arg-parse", check_argparse)
+
+
+def add_argparse(app, parser, subparsers):
+    s = subparsers.add_parser("boot",
+                              help="Manage the bootloader")
+    s.add_argument("--default",
+                   help="Mkae this image the default boot entry")
+    s.add_argument("IMAGE", help="Image to boot")
+
+
+def check_argparse(app, args):
+    log().debug("Operating on: %s" % app.imgbase)
+    if args.command == "boot":
+        raise NotImplemented()
+
+# vim: sw=4 et sts=4
