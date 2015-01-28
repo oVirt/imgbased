c8bcdb32be5f373a8647bdc2350e64e2a485acb9
Fabian Deutsch <fabiand@fedoraproject.org>
2015-01-28 22:29:01 +0100

    build: Switch to force reuse existing rootfs
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/install.sh
+++ ci/install.sh
@@ -7,6 +7,6 @@ export PATH=$PATH:/sbin/:/usr/sbin/
 ./autogen.sh
 ./configure
 
-make image-install
+make image-install FORCE_REUSE_EXISTING_ROOTFS=1
 
 ln -v rootfs.squashfs.img "$(make verrel).squashfs.img"
