83c419c827e933b41e6c1bf0c1bef3a5f4e99961
Fabian Deutsch <fabiand@fedoraproject.org>
2015-01-29 08:55:21 +0100

    ci: Touch rootfs to pretend it was built
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/install.sh
+++ ci/install.sh
@@ -7,6 +7,9 @@ export PATH=$PATH:/sbin/:/usr/sbin/
 ./autogen.sh
 ./configure
 
+# Touch the rootfs.qcow2, to pretend that it is fresh
+[ -f rootfs.qcow2 ] && touch rootfs.qcow2
+
 make image-install
 
 ln -v rootfs.squashfs.img "$(make verrel).squashfs.img"
