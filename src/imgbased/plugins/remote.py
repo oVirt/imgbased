ff2be55ca77666fe69bbf3821ef3ae9d57503046
Fabian Deutsch <fabiand@fedoraproject.org>
2015-01-31 16:19:04 +0100

    remote: Add simple index image discovery
    The plugin can now discover images form remotes, based on simple index
    files.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/imgbased/plugins/remote.py
+++ src/imgbased/plugins/remote.py
@@ -3,8 +3,9 @@ from ..utils import log
 from ConfigParser import ConfigParser
 from StringIO import StringIO
 import shlex
-import tempfile
 import glob
+import requests
+from os import path
 from pprint import pprint
 
 
@@ -45,7 +46,7 @@ def StringIOConfigParser(cfgstr):
         yield (section, dict(p.items(section)))
 
 
-class Remotes():
+class LocalRemotesConfiguration():
     """Datastructure to access localy configured remotes
 
     We configure remote repositories/locations locally,
@@ -60,7 +61,7 @@ class Remotes():
     ... url = http://jenkins.ovirt.org/
     ... '''
 
-    >>> rs = Remotes()
+    >>> rs = LocalRemotesConfiguration()
 
     >>> rs._parse_config(example)
     {'jenkins': <Remote name=jenkins url=http://jenkins.ovirt.org/ \>}
@@ -72,7 +73,7 @@ class Remotes():
         """List all availabel remotes
         """
         remotes = {}
-        for cfg in glob.glob(Remotes.CFGD_PATH + "/*"):
+        for cfg in glob.glob(RemotesConfiguration.CFGD_PATH + "/*"):
             remotes.update(self.parse_config(cfg))
         return remotes
 
@@ -85,7 +86,7 @@ class Remotes():
     def _parse_config(self, cfgstr):
         """Parse a single config file
 
-        >>> rs = Remotes()
+        >>> rs = LocalRemotesConfiguration()
         >>> rs._parse_config("")
         {}
         >>> rs._parse_config("[remote thing]\\nurl = bar")
@@ -106,109 +107,112 @@ class Remotes():
 class Remote(object):
     """Datastructure to operate on some remote repository with images
 
-    Inspired by http://0pointer.net/blog/revisiting-how-we-put-together-linux-systems.html
-
-    >>> example = '''
-    ... [general]
-    ... architecture = x86_64
-    ... vendorid = org.ovirt.Node_UNSTABLE
-    ...
-    ... [image Node-20141010.0]
-    ... version = 20141010.0
-    ...
-    ... [image Node-20141212.0]
-    ... version = 20141212.0
-    ... '''
-
-    >>> r = Remote()
-    >>> r.url = "http://example.com"
-
-    >>> images = r._parse_imageinfo(example)
-
-    >>> pprint(images)
-    [<Image vendorid=org.ovirt.Node_UNSTABLE version=20141010.0 path=Node-20141010.0 \>,
-     <Image vendorid=org.ovirt.Node_UNSTABLE version=20141212.0 path=Node-20141212.0 \>]
-
-    >>> r._fetch_url(images[0])
-    'http://example.com/Node-20141010.0'
-
+    Influenced by
+    http://0pointer.net/blog/revisiting-how-we-put-together-linux-systems.html
     """
     name = None
     url = None
 
-    IMAGEINFO_FILE = ".imageinfo"
+    _discoverer = None
 
-    def _fetch_imageinfo(self):
-        """Fetch the remote imageinfo file and return it's contents
-        """
-        with urllib2.urlopen(self.url + "/" + Remote.IMAGEINFO_FILE) as src:
-            return src.read()
+    def __init__(self):
+        self._discoverer = SimpleIndexImageDiscoverer(self)
 
-    def _parse_imageinfo(self, cfgstr):
-        """Parse an imageinfo file and return the defined images
-
-        >>> r = Remote()
-        >>> r._parse_imageinfo("")
-        []
-        >>> r._parse_imageinfo("[image node-1.tar.xz]\\nversion=1.0")
-        [<Image vendorid=None version=1.0 path=node-1.tar.xz \>]
-        >>> r._parse_imageinfo("[image node-2.tar.xz]\\nversion=2.0")
-        [<Image vendorid=None version=2.0 path=node-2.tar.xz \>]
-        """
-        images = []
-        vendorid = arch = None
-        for section, items in StringIOConfigParser(cfgstr):
-            if section == "general":
-                vendorid = items["vendorid"]
-                arch = items["architecture"]
-
-            if section.startswith("image "):
-                i = ImageInfo()
-                i.path = shlex.split(section)[1]
-                i.vendorid = vendorid
-                i.architecture = arch
-                i.version = items.get("version", None)
-                images.append(i)
-        return images
-
-    def _fetch_url(self, imageinfo):
-        """Return the URL to fetch the given image from
-        """
-        return self.url + "/" + imageinfo.path
+    def list_images(self):
+        return self._discoverer.list_images()
 
     def __repr__(self):
         return "<Remote name=%s url=%s \>" % \
             (self.name, self.url)
 
-    def list_images(self):
-        """List all available images at this remote
-        """
-        cfgstr = self._fetch_imageinfo()
-        return self._parse_imageinfo(cfgstr)
 
-    def pull(self, imageinfo):
-        """Fetch and store a remote image
-        """
-        # fixme add hash
-        #with open("FIXME", "wb") as dst:
-        #    with urllib2.urlopen(self.url + "/" + imageinfo.path) as src:
-        #        dst.write(src.read(1024))
-        print path
-        #curl("--location", "--fail",
-        #     "--output", imgbase
-
-
-class ImageInfo():
+class RemoteImage():
     """Represents informations about a single remote image
+
+    path: Is relative, a filename
     """
+    reomte = None
+
     vendorid = None
     architecture = None
     version = None
     path = None
 
-    def __repr__(self):
-        return "<Image vendorid=%s version=%s path=%s \>" % \
-            (self.vendorid, self.version, self.path)
+    def __init__(self, remote):
+        self.remote = remote
 
+    def __repr__(self):
+        return "<Image name=%s vendorid=%s version=%s path=%s \>" % \
+            (self.name, self.vendorid, self.version, self.path)
+
+    def url(self):
+        return self.remote.url + "/" + self.path
+
+    def pull(self, dstpath):
+        """Fetch and store a remote image
+
+        dstpath: device or filename
+        """
+        url = self.url()
+        req = requests.get(url)
+        req.raise_for_status()
+        with open(dstpath, "wb") as dst:
+            for chunk in req.iter_content(1024):
+                dst.write(chunk)
+        #curl("--location", "--fail",
+        #     "--output", imgbase
+
+
+class SimpleIndexImageDiscoverer():
+    """Remotely find images based on a simple index file
+
+    >>> example = '''
+    ... # A comment
+    ... rootfs:<name>:<vendor>:<arch>:<version>.<suffix>
+    ... '''
+
+    >>> r = SimpleIndexImageDiscoverer(None)
+
+    >>> r._list_images(example.split("\\n"))
+    [<Image name=<name> vendorid=<vendor> version=<version> \
+path=rootfs:<name>:<vendor>:<arch>:<version>.<suffix> \>]
+    """
+    indexfile = ".index"
+
+    def __init__(self, remote):
+        self.remote = remote
+
+    def _imageinfo_from_name(self, filename):
+        parts = filename.split(":")
+
+        assert parts.pop(0) == "rootfs", "Only supporting rootfs images"
+
+        info = RemoteImage(self.remote)
+        info.path = filename
+        info.name = parts.pop(0)
+        info.vendorid = parts.pop(0)
+        info.arch = parts.pop(0)
+        info.version = parts.pop(0).split(".", 1)[0] # Strip an eventual suffix
+
+        return info
+
+    def _list_images(self, lines):
+        filenames = []
+        images = []
+        for line in lines:
+            if line and not line.startswith("#"):
+                filenames.append(line)
+        for filename in filenames:
+            try:
+                images.append(self._imageinfo_from_name(filename))
+            except Exception as e:
+                log().info("Failed to parse imagename '%s': %s" %
+                           (filename, e))
+        return images
+
+    def list_images(self):
+        src = requests.get(self.remote.url + "/" + self.indexfile).text
+        lines = src.split("\n")
+        return self._list_images(lines)
 
 # vim: sw=4 et sts=4
