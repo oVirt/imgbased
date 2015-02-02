2a5af9a6824018c0b8b0f8f7c68be9de63cea388
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-03 00:34:41 +0100

    remote: Add CLI
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/imgbased/plugins/remote.py
+++ src/imgbased/plugins/remote.py
@@ -7,6 +7,8 @@ import glob
 import requests
 from os import path
 from pprint import pprint
+import argparse
+import sys
 
 
 def init(app):
@@ -21,10 +23,26 @@ def add_argparse(app, parser, subparsers):
     s = subparsers.add_parser("remote",
                               help="Fetch images from remote sources")
 
-    s.add_argument("--nightly", action="store_true", help="Nightly image")
-    s.add_argument("--stable", action="store_true", help="Stable image")
+    su = s.add_subparsers(help="Comands to operate on remotes", dest="subcmd")
 
-    # FIXME pull from jenkins based on config file
+    su_add = su.add_parser("add", help="Add a remote")
+    su_add.add_argument("NAME", type=str)
+    su_add.add_argument("URL", type=str)
+    su_remove = su.add_parser("remove", help="Remove a remote")
+    su_remove.add_argument("NAME", type=str)
+    su_list = su.add_parser("list", help="List availabel remote images")
+    su_fetch = su.add_parser("fetch", help="Retireve a remote image into "
+                             "a dst")
+    su_fetch.add_argument("NAME", type=str)
+    su_fetch.add_argument("IMAGE", type=str)
+    su_fetch.add_argument("DEST", nargs="?", type=argparse.FileType('r'),
+                          default=sys.stdin,
+                          help="File or stdin to use")
+
+    su_pull = su.add_parser("pull", help="Pull a remote image and add it "
+                            "to the layout")
+    su_fetch.add_argument("NAME", type=str)
+    su_fetch.add_argument("IMAGE", type=str)
 
 
 def check_argparse(app, args):
@@ -32,19 +50,21 @@ def check_argparse(app, args):
     It will be called when the user selects a sub-command
     """
     log().debug("Operating on: %s" % app.imgbase)
-    if args.command == "remote":
-        raise NotImplemented()
+    if args.command != "remote":
+        # Not us
+        return
 
+    remotes = LocalRemotesConfiguration()
 
-def StringIOConfigParser(cfgstr):
-    """A config parser which reads a string
-    """
-    p = ConfigParser()
-    p.readfp(StringIO(cfgstr))
+    if args.subcmd == "add":
+        remotes.add(args.NAME, args.URL)
 
-    for section in p.sections():
-        yield (section, dict(p.items(section)))
+    elif args.subcmd == "remove":
+        remotes.remove(args.NAME)
 
+    elif args.subcmd == "list":
+        for name, url in sorted(remotes.list().items()):
+            print "%s: %s" % (name, url)
 
 class LocalRemotesConfiguration():
     """Datastructure to access localy configured remotes
@@ -62,46 +82,96 @@ class LocalRemotesConfiguration():
     ... '''
 
     >>> rs = LocalRemotesConfiguration()
+    >>> rs.cfgstr = example
 
-    >>> rs._parse_config(example)
+    >>> rs.list()
     {'jenkins': <Remote name=jenkins url=http://jenkins.ovirt.org/ \>}
-
     """
-    CFGD_PATH = "/tmp/remotes.d"
+    CFG_PATH = "/tmp/remotes_config"
+    cfgstr = None
+
+    def _parser(self):
+        p = ConfigParser()
+        if self.cfgstr:
+            # Used for doctests
+            p.readfp(StringIO(self.cfgstr))
+        else:
+            p.read(self.CFG_PATH)
+        return p
+
+    def _iter_sections(self):
+        """A config parser which reads a string
+
+        >>> example = '''
+        ... [foo]
+        ... a = 1
+        ... [bar]
+        ... b = 2
+        ... '''
+
+        >>> rs = LocalRemotesConfiguration()
+        >>> rs.cfgstr = example
+        >>> list(rs._iter_sections())
+        [('foo', {'a': '1'}), ('bar', {'b': '2'})]
+        """
+        p = self._parser()
+        for section in p.sections():
+            yield (section, dict(p.items(section)))
 
     def list(self):
         """List all availabel remotes
-        """
-        remotes = {}
-        for cfg in glob.glob(RemotesConfiguration.CFGD_PATH + "/*"):
-            remotes.update(self.parse_config(cfg))
-        return remotes
 
-    def parse_config(self, filename):
-        """Parse a single remote config file
-        """
-        with open(filename) as src:
-            return self._parse_config(src.read())
-
-    def _parse_config(self, cfgstr):
-        """Parse a single config file
+        >>> example = '''
+        ... [general]
+        ...
+        ... [remote jenkins]
+        ... url = http://jenkins.ovirt.org/
+        ... '''
 
         >>> rs = LocalRemotesConfiguration()
-        >>> rs._parse_config("")
+        >>> rs.cfgstr = example
+
+        >>> rs.list()
+        {'jenkins': <Remote name=jenkins url=http://jenkins.ovirt.org/ \>}
+
+        >>> rs = LocalRemotesConfiguration()
+        >>> rs.cfgstr = ""
+        >>> rs.list()
         {}
-        >>> rs._parse_config("[remote thing]\\nurl = bar")
+
+        >>> rs.cfgstr = "[remote thing]\\nurl = bar"
+        >>> rs.list()
         {'thing': <Remote name=thing url=bar \>}
-        >>> rs._parse_config("[remote 'a thing']\\nurl = bar")
+
+        >>> rs.cfgstr = "[remote 'a thing']\\nurl = bar"
+        >>> rs.list()
         {'a thing': <Remote name=a thing url=bar \>}
         """
         remotes = {}
-        for section, items in StringIOConfigParser(cfgstr):
+        for section, items in self._iter_sections():
             if section.startswith("remote "):
                 r = Remote()
                 r.name = shlex.split(section)[1]
                 r.url = items["url"]
                 remotes[r.name] = r
         return remotes
+
+    def add(self, name, url):
+        p = self._parser()
+        section = "remote %s" % name
+        p.add_section(section)
+        p.set(section, "url", url)
+        self._save(p)
+
+    def remove(self, name):
+        p = self._parser()
+        section = "remote %s" % name
+        p.remove_section(section)
+        self._save(p)
+
+    def _save(self, p):
+        with open(self.CFG_PATH, 'wb') as configfile:
+            p.write(configfile)
 
 
 class Remote(object):
