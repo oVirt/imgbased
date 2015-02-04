248561ca230aaf9fe968c7efd75d00b676c50f06
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-04 19:53:33 +0100

    ci: Name ks properly
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/install.sh
+++ ci/install.sh
@@ -15,4 +15,11 @@ make image-install SQUASHFS_URL=$SQUASHFS_URL
 IMG="$(make verrel).squashfs.img"
 ln -v rootfs.squashfs.img $IMG
 
+# Create an index file for imgbase remote
 echo $IMG > .index
+
+# Create a kickstart for a specific location
+cp -v data/images/kickstarts/installation.ks auto-installation.ks
+
+# Create an inteactive kickstart for a specific location
+sed -e "/^clearpart / d" data/images/kickstarts/installation.ks > interactive-installation.ks
