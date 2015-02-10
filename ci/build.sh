b151f435a2a8fb37a8720d7cb998fcf63fd0ca4b
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-10 11:49:41 +0100

    ci: Add glusterd support
    Allow adding gluster packages from the official gluster repositories.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/build.sh
+++ ci/build.sh
@@ -16,12 +16,31 @@ then
   log "Including vdsm from the oVirt repositories"
   cat <<EOF >> data/images/kickstarts/template/rootfs.ks
 
+# Adding vdsm
 %pre
 yum install -y http://resources.ovirt.org/pub/yum-repo/ovirt-release35.rpm
 %end
-
 %packages
 vdsm
+%end
+EOF
+fi
+
+if ${WITH_GLUSTER:-false}
+then
+  log "Including gluster from http://www.gluster.org"
+  cat <<EOF >> data/images/kickstarts/template/rootfs.ks
+
+# Adding gluster
+%pre
+yum-config-manager --add-repo="http://download.gluster.org/pub/gluster/glusterfs/LATEST/Fedora/glusterfs-fedora.repo"
+%end
+%packages
+# Taken from http://www.gluster.org/documentation/quickstart/index.html
+glusterfs
+glusterfs-server
+glusterfs-fuse
+glusterfs-geo-replication
 %end
 EOF
 fi
