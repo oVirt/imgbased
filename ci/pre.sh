9b938f303629b3ec115d522c4e034d3231f44005
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-17 18:09:06 +0100

    ci: Add squashfs-tools
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/pre.sh
+++ ci/pre.sh
@@ -6,5 +6,5 @@ export PATH=$PATH:/sbin:/usr/sbin
 export TMPDIR=/var/tmp/
 log() { echo -e "\n\n$@\n\n" ; }
 
-sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster
+sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster squashfs-tools
 git submodule update --init --recursive
