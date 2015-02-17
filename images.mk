705addad46a226586acddc48f921d85ccb8a1e6a
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-17 16:32:13 +0100

    Refactor image building
    Split most tools out, and leave the dependencies in the makefiles
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- images.mk
+++ images.mk
@@ -4,65 +4,33 @@
 # files
 #
 
-NAME = rootfs
-SQUASHFS=$(NAME).squashfs.img
-QCOW = $(NAME).qcow2
-QCOW_CHECK = installation.qcow2
+KICKSTARTS=$(wildcard data/images/kickstarts/*/*.ks)
+.SECONDARY: rootfs.qcow2
 
-.SECONDARY : rootfs.qcow2
+image-build: rootfs.qcow2
 
-#
-# Build the test image
-# and sparsify if possible
-#
-image-build: $(QCOW)
+%.qcow2: $(KICKSTARTS)
+	make -C data/images kickstarts/$*.ks
+	mv -vf data/images/kickstarts/$*.ks
+	make -f tools/build.mk $@
+	-virt-sparsify --check-tmpdir continue --compress $@ $@.sparse && mv -v $@.sparse $@
 
-clean-build:
-	cd data/images && make clean
+%.squashfs.img: %.qcow2
+	 make -f tools/build.mk $@
+	unsquashfs -ll $@
 
-$(QCOW): $(PARTIAL_KS)
-	cd data/images && make run-install DISK_NAME=$(QCOW) KICKSTART=kickstarts/$(NAME).ks
-	mv -v data/images/$(QCOW) $(srcdir)
-	-virt-sparsify --check-tmpdir continue --compress $(QCOW) $(QCOW).sparse && mv -v $(QCOW).sparse $(QCOW)
+image-install:
+	[[ -f rootfs.squashfs.img ]]
+	-rm -f data/images/kickstarts/installation.ks
+	$(MAKE) -C data/images kickstarts/installation.ks
+	mv -vf data/images/kickstarts/installation.ks .
+	sed -i "s#@ROOTFS_URL@#@HOST_HTTP@/rootfs.squashfs.img#" installation.ks
+	$(MAKE) -f tools/build.mk installation.qcow2
 
-#
-# Now some targets to test the installation part
-#
-rootfs.raw: rootfs.qcow2
-	qemu-img convert -p -S 1M -O raw $< $@
-
-rootfs.squashfs.img: rootfs.raw
-	bash tools/image_to_squashfs $< $@
-
-rootfs.tar.xz: rootfs.qcow2
-	if [[ -e $@ ]]; then echo "Tarball already exists" ; else guestfish -i -a $< tar-out / $@ compress:xz ; fi
-
-image-install: SQUASHFS_URL=http://10.0.2.2:\$$(PYPORT)/
-image-install: $(SQUASHFS)
-	[[ -f "$(SQUASHFS)" ]]
-	-rm -f data/images/kickstarts/installation.ks data/images/$(SQUASHFS)
-	-ln -s $$PWD/$(SQUASHFS) data/images/
-	$(MAKE) image-build NAME=installation SED_KS="s#@ROOTFS_URL@#$(SQUASHFS_URL)/$(SQUASHFS)#"
-
-verrel: TYPE=rootfs
-verrel: NAME=FedoraNodeNext
-verrel: VENDOR=org.ovirt.node
-verrel: ARCH=x86_64
-verrel: VERSION=$$(date +%Y%m%d)$(EXTRA_RELEASE)
 verrel:
-	@bash tools/image-verrel $(TYPE) $(NAME) $(VENDOR) $(ARCH) $(VERSION)
+	@bash tools/image-verrel rootfs NodeNext org.ovirt.node
 
-#
-# Run simple and advanced test
-#
+check: QCOW_CHECK=installation.qcow2
 check:
-	[[ -f "$(QCOW_CHECK)" ]] && $(MAKE) check-runtime || :
-
-#
-# Run runtime/functional test on the test image
-# Intentioanlly no dependency on build
-#
-check-runtime: $(QCOW_CHECK)
-	make -f tests/runtime/Makefile check-local IMAGE=$(QCOW_CHECK)
-
+	[[ -f "$(QCOW_CHECK)" ]] && make -f tests/runtime/Makefile check-local IMAGE=$(QCOW_CHECK) || :
 
