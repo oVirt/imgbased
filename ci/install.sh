0838f02131508ba413265c4a8545e13e9afb4b45
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-17 17:43:51 +0100

    ci: Cleanup
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/install.sh
+++ ci/install.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/sbin/:/usr/sbin/
 ./autogen.sh
 ./configure
 
-make image-install SQUASHFS_URL=$SQUASHFS_URL
+[[ -n $SQUASHFS_URL ]] && make image-install SQUASHFS_URL=$SQUASHFS_URL || make image-install
 
 IMG="$(make verrel).squashfs.img"
 ln -v rootfs.squashfs.img $IMG
