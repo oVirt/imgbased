4bbe5f9124afd736de666585b0871739de78fcc0
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-19 13:40:52 +0100

    ci: Remove unneeded function
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/pre.sh
+++ ci/pre.sh
@@ -4,7 +4,6 @@ set -ex
 
 export PATH=$PATH:/sbin:/usr/sbin
 export TMPDIR=/var/tmp/
-log() { echo -e "\n\n$@\n\n" ; }
 
 sudo yum -y install libguestfs-tools qemu-system-x86 asciidoc python-sh glusterfs gluster squashfs-tools
 git submodule update --init --recursive
