ff52688e71425b9ea650f44b12d13b736788921a
Fabian Deutsch <fabiand@fedoraproject.org>
2015-05-17 21:15:21 +0200

    Move layer to module
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/imgbased/__main__.py
+++ src/imgbased/__main__.py
@@ -84,15 +84,6 @@ if __name__ == '__main__':
     init_group.add_argument("--without-vg", action="store_true", default=False,
                             help="Do not create a Volume Group")
 
-    layer_parser = subparsers.add_parser("layer",
-                                         help="Runtime layer handling")
-    layer_parser.add_argument("--add", action="store_true",
-                              default=False, help="Add a new layer")
-    layer_parser.add_argument("--latest", action="store_true",
-                              help="Get the latest layer")
-    layer_parser.add_argument("--current", action="store_true",
-                              help="Get the current layer used to boot this")
-
     app.hooks.emit("pre-arg-parse", parser, subparsers)
 
     args = parser.parse_args()
@@ -124,14 +115,6 @@ if __name__ == '__main__':
             print(app.imgbase.free_space(args.units))
         else:
             print(app.imgbase.layout())
-
-    elif args.command == "layer":
-        if args.add:
-            app.imgbase.add_bootable_layer()
-        elif args.current:
-            print (app.imgbase.current_layer())
-        elif args.latest:
-            print (app.imgbase.latest_layer())
 
     #
     # Now let the plugins check if they need to run something
---
+++ src/imgbased/plugins/layer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# imgbase
+#
+# Copyright (C) 2014  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Fabian Deutsch <fabiand@redhat.com>
+#
+from ..utils import log
+
+
+def init(app):
+    app.hooks.connect("pre-arg-parse", add_argparse)
+    app.hooks.connect("post-arg-parse", check_argparse)
+
+
+def add_argparse(app, parser, subparsers):
+    layer_parser = subparsers.add_parser("layer",
+                                         help="Runtime layer handling")
+    layer_parser.add_argument("--add", action="store_true",
+                              default=False, help="Add a new layer")
+    layer_parser.add_argument("--latest", action="store_true",
+                              help="Get the latest layer")
+    layer_parser.add_argument("--current", action="store_true",
+                              help="Get the current layer used to boot this")
+
+
+def check_argparse(app, args):
+    log().debug("Operating on: %s" % app.imgbase)
+    if args.command == "layer":
+        if args.add:
+            app.imgbase.add_bootable_layer()
+        elif args.current:
+            print (app.imgbase.current_layer())
+        elif args.latest:
+            print (app.imgbase.latest_layer())
+
+# vim: sw=4 et sts=4
