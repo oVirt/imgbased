4498052c56684f803b33d1cda0cfd20ef9cb201f
Fabian Deutsch <fabiand@fedoraproject.org>
2015-06-23 08:11:27 +0200

    osupdater: Name initramfs correctly
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/imgbased/plugins/osupdater.py
+++ src/imgbased/plugins/osupdater.py
@@ -241,7 +241,7 @@ def adjust_mounts_and_boot(imgbase, new_layer, previous_layer):
         kver = kvers.pop()
         log.debug("Found kvers: %s" % kvers)
         log.debug("Using kver: %s" % kver)
-        initrd = "/boot/initramfs-%s" % kver
+        initrd = "/boot/initramfs-%s.img" % kver
         chroot("dracut", "-f", initrd, "--kver", kver)
 
     def add_bootentry(newroot):
