bfafa4433f6f06af268e07ca9195308d348e2ad9
Fabian Deutsch <fabiand@fedoraproject.org>
2015-12-20 21:36:39 +0100

    test: Refactor testing to use FakeLVM
    Previously the testing required to be run in a VM with
    a fully functional LVM available.
    
    TO ease testing now a fake LVM (FakeLVM) class is used to
    patch out the imgbased.lvm.LVM class.
    It mimics a bug free LVM behavior and can be used to validate
    the high-level logic of imgbased.
    
    Benefits are:
    - Does not require root privileges
    - Independent of LVM presence and layout
    - Pure pythonic
    - Fast
    
    Change-Id: I40dba8ad07203bdc5618a63ea81d259ba05003a1
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/runtime/testSanity.py
+++ tests/runtime/testSanity.py
@@ -2,34 +2,192 @@
 # vim: et ts=4 sw=4 sts=4
 
 import unittest
-import sh
 import logging
+from mock import patch
 
+import imgbased.__main__
+import imgbased.lvm
 
-# Increase the capture length of python-sh to show complete errors
-sh.ErrorReturnCode.truncate_cap = 999999
 
 log = logging.info
 
 
+class FakeLVM(imgbased.lvm.LVM):
+    _vgs = []
+
+    @staticmethod
+    def lvs():
+        lvs = []
+        for vg in FakeLVM._vgs:
+            lvs.extend(lv.lv_name for lv in vg._lvs)
+        return lvs
+
+    class VG(imgbased.lvm.LVM.VG):
+        vg_name = None
+        _tags = None
+        _pvs = None
+        _lvs = None
+
+        def __init__(self):
+            self._tags = set()
+            self._pvs = set()
+            self._lvs = set()
+
+        @staticmethod
+        def find_by_tag(tag):
+            return [g for g in FakeLVM._vgs
+                    if tag in g.tags()]
+
+        @staticmethod
+        def from_tag(tag):
+            vgs = FakeLVM.VG.find_by_tag(tag)
+            assert len(vgs) == 1
+            return vgs[0]
+
+        @staticmethod
+        def create(vg_name, pv_paths):
+            vg = FakeLVM.VG()
+            vg.vg_name = vg_name
+            vg._pvs = pv_paths
+            return vg
+
+        def create_thinpool(self, name, size):
+            thinlv = FakeLVM.LV()
+            thinlv.vg_name = self.vg_name
+            thinlv.lv_name = name
+            thinlv._thin = True
+            thinlv._pool = True
+            thinlv._size = size
+            self._lvs.append(thinlv)
+            return thinlv
+
+        def addtag(self, tag):
+            self._tags.add(tag)
+
+        def tags(self):
+            return self._tags
+
+    class LV(imgbased.lvm.LVM.LV):
+        vg_name = None
+        lv_name = None
+
+        _thin = False
+        _pool = False
+        _size = None
+        _origin = None
+        _active = True
+        _activationskip = False
+        _permission = "rw"
+
+        @property
+        def path(self):
+            raise NotImplementedError()
+
+        @staticmethod
+        def find_by_tag(tag):
+            return [v for v in FakeLVM.lvs()
+                    if tag in v.tags()]
+
+        @staticmethod
+        def from_path(path):
+            raise NotImplementedError()
+
+        def create_snapshot(self, new_name):
+            lv = FakeLVM.LV(self.vg_name, new_name)
+            lv._thin = self._thin
+            lv._size = self._size
+            lv._origin = self
+            lv._active = False if self._thin else True
+            lv._activationskip = True if self._thin else False
+            lv._permission = self._permission
+            self._vgs.append(lv)
+
+        def remove(self, force=False):
+            raise NotImplementedError()
+
+        def activate(self, val, ignoreactivationskip=False):
+            assert val in [True, False]
+            if not self._activationskip:
+                self._active = val
+            elif self._activationskip and ignoreactivationskip:
+                self._active = val
+
+        def setactivationskip(self, val):
+            assert val in [True, False]
+            self._activationskip = val
+
+        def permission(self, val):
+            assert val in ["r", "rw"]
+            self._permission = val
+
+        def thinpool(self):
+            raise NotImplementedError()
+
+        def addtag(self, tag):
+            self._tags.add(tag)
+
+        def tags(self):
+            return self._tags
+
+        def origin(self):
+            return self._origin
+
+        def options(self, options):
+            raise NotImplementedError()
+
+    class Thinpool(LV):
+        _virtualsize = None
+
+        def create_thinvol(self, vol_name, volsize):
+            lv = FakeLVM.LV(self.vg_name, vol_name)
+            lv.size = volsize
+            lv._virtualsize = volsize
+            lv._thin = True
+            return lv
+
+
+@patch("imgbased.lvm.LVM", FakeLVM)
+@patch("imgbased.imgbase.LVM", FakeLVM)
+def imgbase(*args):
+    try:
+        imgbased.__main__.CliApplication(args)
+    except SystemExit as e:
+        if e.code != 0:
+            raise
+
+
+def autopart():
+    vg = FakeLVM.VG()
+    vg.vg_name = "hostvg"
+    vg._pvs = ["/dev/sda", "/dev/sdb"]
+
+    lv0 = FakeLVM.LV(vg.vg_name, "Image-1.0")
+    lv1 = FakeLVM.LV(vg.vg_name, "Image-1.1")
+    vg._lvs = [lv0, lv1]
+
+    FakeLVM._vgs = [vg]
+    print(FakeLVM.lvs())
+
+
 class TestImgbased(unittest.TestCase):
-    def test_imgbase(self):
-        from sh import imgbase, lvm
 
-        # All subsequent imgbase calls include the debug arg
-        imgbase = imgbase.bake("--debug")
+    def setUp(self):
+        autopart()
 
-        log("Using %s" % imgbase)
+    def test_layout(self):
         log(imgbase("--version"))
+        imgbase("layout")
 
-        log("Existing LVM layout")
-        log(lvm.pvs())
-        log(lvm.vgs())
-        log(lvm.lvs())
+    def test_base(self):
+        with self.assertRaises(RuntimeError):
+            imgbase("base", "--add", "Bar")
 
-        assert "HostVG" in lvm.vgs()
+        imgbase("base", "--add", "Bar", "--size", "4096")
 
-        assert "Image-0.0" in imgbase.layout()
+        print(FakeLVM.lvs())
 
-        # Check that a current layer exists
-        imgbase.layer("--current")
+
+if __name__ == "__main__":
+    unittest.main()
+
+# vim: sw=4 et sts=4
