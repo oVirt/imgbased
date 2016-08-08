88cf4b57e3f533abc568d5baa6d6e4aca9958632
Fabian Deutsch <fabiand@fedoraproject.org>
2016-08-08 13:08:48 +0200

    Revert "imgbase: Drop journal support"
    This reverts commit 0467ef3529a3b2f54e3e4db0b02798fde9a6fd04.
    
    Change-Id: Ib23d920f7e93e285b899dbb66611cba949113bc7
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1364037
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/imgbased/__main__.py
+++ src/imgbased/__main__.py
@@ -32,7 +32,12 @@ if __name__ == '__main__':
     lvl = logging.DEBUG if "--debug" in sys.argv else logging.INFO
     fmt = "[%(levelname)s] %(message)s"
 
-    h = logging.StreamHandler()
+    try:
+        from systemd import journal
+        h = journal.JournalHandler(SYSLOG_IDENTIFIER=config.PACKAGE_NAME)
+    except:
+        h = logging.StreamHandler()
+
     h.setLevel(lvl)
     h.setFormatter(logging.Formatter(fmt))
 
