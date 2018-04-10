f6a1f1d8aa381092625053dbb6a2518d24827bf4
Yuval Turgeman <yturgema@redhat.com>
2018-04-10 13:12:40 +0300

    Add /dev/urandom to new layer
    Some binaries that depend on nss-softkn-freebl (like rpm and some python
    modules) fail to run inside chroot when the urandom device is missing,
    so we add it to the new lv before continuing the install/update
    procedure.
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1560138
    Change-Id: Idca6aa8260aa3f54096815a3d47095d490f32e7e
    Signed-off-by: Yuval Turgeman <yturgema@redhat.com>

--- src/imgbased/utils.py
+++ src/imgbased/utils.py
@@ -456,6 +456,9 @@ class ExternalBinary(object):
     def getenforce(self):
         return self.call(["getenforce"])
 
+    def mknod(self, args, **kwargs):
+        return self.call(["mknod"] + args, **kwargs)
+
 
 class LvmBinary(ExternalBinary):
     def call(self, *args, **kwargs):
